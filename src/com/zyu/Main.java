4994001ab8f51129f8bba9d8c78a03a533c6efd5
Yevhenii Zakharenkov <evgen.fw@gmail.com>
2020-01-31 21:17:59 +0200

    call plus() method

--- src/com/zyu/Main.java
+++ src/com/zyu/Main.java
@@ -1,18 +1,42 @@
 package com.zyu;
 
+import java.util.Scanner;
+
 public class Main {
 
     public void initialization(){
-        Calculator calc = new Calculator();
+
     }
+
     public void run(){
+        Scanner scanner = new Scanner(System.in);
+        Calculator calc = new Calculator("Culc run!");
 
+        while (true) {
+
+            System.out.print("Operand a: ");
+            calc.operand1 = scanner.nextFloat();
+
+            System.out.print("Operation: ");
+            calc.operation = scanner.next();
+
+            System.out.print("Operand b: ");
+            calc.operand2 = scanner.nextFloat();
+
+            if(calc.operation.equals("+")) {
+                System.out.println(calc.plus(calc.operand1, calc.operand2));
+            }
+        }
     }
-
 
     public static void main(String[] args) {
         Main main = new Main();
         main.initialization();
+
+        /*Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        String s = scanner.nextLine();*/
+
         main.run();
     }
 }
