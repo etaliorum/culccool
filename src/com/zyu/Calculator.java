b7e5605564c5d0975620af0003567b1499fb1078
Yevhenii Zakharenkov <evgen.fw@gmail.com>
2020-01-31 20:09:37 +0200

    aded methods base operation

--- src/com/zyu/Calculator.java
+++ src/com/zyu/Calculator.java
@@ -1,5 +1,32 @@
 package com.zyu;
 
 public class Calculator {
+    String operation;
+    float operand1;
+    float operand2;
+
+    public Calculator(String run) {
+        System.out.println(run);
+    }
+
+    public float plus(float a, float b){
+
+        return a + b;
+    }
+
+    public float minus(float a, float b){
+
+        return a - b;
+    }
+
+    public float div(float a, float b){
+
+        return a / b;
+    }
+
+    public float multiplie(float a, float b) {
+
+        return a * b;
+    }
 
 }
